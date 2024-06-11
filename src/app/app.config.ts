b9e0fa10940d6b0a5be81a2cd4c859e645b6a906
dantorval-us <dantorval@alum.us.es>
2024-06-11 17:20:27 +0200

    bd/fix comenta locationId de provideFirebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,17 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"gespana-d847c","appId":"1:554839476562:web:344795c52b980956f4528b","storageBucket":"gespana-d847c.appspot.com","locationId":"europe-west","apiKey":"AIzaSyCLkabivrXY4mXFnmP8sLkSVGl2vKBuJjA","authDomain":"gespana-d847c.firebaseapp.com","messagingSenderId":"554839476562"})), provideStorage(() => getStorage())]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideFirebaseApp(() => initializeApp({
+      "projectId":"gespana-d847c",
+      "appId":"1:554839476562:web:344795c52b980956f4528b",
+      "storageBucket":"gespana-d847c.appspot.com",
+      // "locationId":"europe-west",
+      "apiKey":"AIzaSyCLkabivrXY4mXFnmP8sLkSVGl2vKBuJjA",
+      "authDomain":"gespana-d847c.firebaseapp.com",
+      "messagingSenderId":"554839476562"
+    })), 
+    provideStorage(() => getStorage())]
 };
